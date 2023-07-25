0227b93a32d4ad80dbc0582e0d517d9225a4f4d8
applet <celbever@naver.com>
2023-07-25 17:18:11 +0900

    project initalize

---
+++ src/main/java/com/example/jdnc_library/JdncLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdnc_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdncLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdncLibraryApplication.class, args);
+    }
+
+}
