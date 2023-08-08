fecf349e53af8ca7b67b69f630caa7d3c7bd043e
applet <celbever@naver.com>
2023-08-08 15:21:29 +0900

    [feature/#8] entity 추적

--- src/main/java/com/example/jdnc_library/config/JpaConfig.java
+++ src/main/java/com/example/jdnc_library/config/JpaConfig.java
@@ -1,10 +1,13 @@
 package com.example.jdnc_library.config;
 
+import com.example.jdnc_library.domain.member.model.Member;
+import com.example.jdnc_library.security.model.PrincipalDetails;
 import java.util.Optional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
@@ -12,13 +15,16 @@ import org.springframework.security.core.context.SecurityContextHolder;
 public class JpaConfig {
 
     @Bean
-    public AuditorAware<Long> auditorProvider() {
+    public AuditorAware<Member> auditorProvider() {
 
         // 익명 클래스를 이용
-        return new AuditorAware<Long>() {
+        return new AuditorAware<Member>() {
             @Override
-            public Optional<Long> getCurrentAuditor() {
-                return Optional.empty();
+            public Optional<Member> getCurrentAuditor() {
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                if (authentication == null) return Optional.empty();
+                if (!(authentication.getPrincipal() instanceof PrincipalDetails principalDetails)) return Optional.empty();
+                return Optional.of(principalDetails.getMember());
             }
         };
     }
--- src/main/java/com/example/jdnc_library/domain/WriterEntity.java
+++ src/main/java/com/example/jdnc_library/domain/WriterEntity.java
@@ -1,8 +1,12 @@
 package com.example.jdnc_library.domain;
 
+import com.example.jdnc_library.domain.member.model.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -14,9 +18,12 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class WriterEntity extends TimeEntity {
 
     @CreatedBy
-    @Column(updatable = false)
-    private Long createdBy;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by")
+    private Member createdBy;
 
     @LastModifiedBy
-    private Long modifiedBy;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "modified_by")
+    private Member modifiedBy;
 }
