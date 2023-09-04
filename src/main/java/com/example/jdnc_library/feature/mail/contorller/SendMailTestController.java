05b0c490e24dfb481cd684ca9827d91051cedf19
simsimhekoon <37258541+simsimhekoon@users.noreply.github.com>
2023-09-04 12:50:58 +0900

    smtp 기능 수정

--- src/main/java/com/example/jdnc_library/feature/mail/contorller/SendMailTestController.java
+++ src/main/java/com/example/jdnc_library/feature/mail/contorller/SendMailTestController.java
@@ -1,14 +1,11 @@
 package com.example.jdnc_library.feature.mail.contorller;
 
-import com.example.jdnc_library.feature.mail.DTO.MailTemplateDTO;
 import com.example.jdnc_library.feature.mail.service.EmailService;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
@@ -17,18 +14,10 @@ public class SendMailTestController {
 
     private final EmailService emailService;
 
-    @GetMapping("/mail")
-    @ResponseStatus(HttpStatus.OK)
-    public MailTemplateDTO GetMailTemplate() {
-        MailTemplateDTO mailTemplate = emailService.GetMailTemplate();
-        return mailTemplate;
-    }
-
     @PostMapping("/mail")
     @ResponseStatus(HttpStatus.OK)
-    public void SendMailTest(@RequestBody MailTemplateDTO mailTemplateDTO) throws MessagingException {
-        emailService.SendMail(mailTemplateDTO);
-        emailService.SetMailTemplate(mailTemplateDTO);
+    public void SendMailTest() throws MessagingException {
+        emailService.SendMail();
     }
 
 }
---
+++ src/main/java/com/example/jdnc_library/feature/mail/service/MakeMailTemplate.java
@@ -0,0 +1,36 @@
+package com.example.jdnc_library.feature.mail.service;
+
+import com.example.jdnc_library.domain.book.model.BorrowInfo;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MakeMailTemplate {
+    public String getMailTitle() {
+        StringBuffer stringBuffer = new StringBuffer();
+        LocalDate currentDate = LocalDate.now();
+        int currentMonth = currentDate.getMonthValue();
+
+        stringBuffer.append("더큰내일센터 ").append(currentMonth).append("월 도서 미반납자 대상 반납 협조 요청");
+        return stringBuffer.toString();
+    }
+
+    public String getMailTemplate(String managerName, BorrowInfo borrowInfo) {
+        StringBuffer stringBuffer = new StringBuffer();
+        String userName = borrowInfo.getCreatedBy().getName();
+        String bookTitle = borrowInfo.getCollectionInfo().getBookInfo().getTitle();
+        Long bookNum = borrowInfo.getCollectionInfo().getBookNumber();
+        LocalDateTime rentalDate = borrowInfo.getCreatedAt();
+
+        stringBuffer.append("안녕하세요 ").append(userName).append("님").append("\n")
+            .append("제주더큰내일센터 ").append(managerName).append(" 매니저 입니다.").append("\n")
+            .append("도서 미반납이 확인되어 반납 요청 드립니다.\n\n")
+            .append("도서명 : ").append(bookTitle).append("\n")
+            .append("도서 코드번호 : ").append(bookNum).append("\n")
+            .append("대여일자 : ").append(rentalDate).append("\n\n")
+            .append("감사합니다.");
+
+        return stringBuffer.toString();
+    }
+}
