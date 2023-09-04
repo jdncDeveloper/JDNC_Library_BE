53cc028ef5dc55471ae7a4365a438849405030b0
applet <celbever@naver.com>
2023-09-04 15:58:41 +0900

    [hotfix/BookGroupEnumConvertFix] Enum 조회 불가 처리 수정

--- src/main/java/com/example/jdnc_library/domain/book/model/BookGroup.java
+++ src/main/java/com/example/jdnc_library/domain/book/model/BookGroup.java
@@ -2,19 +2,9 @@ package com.example.jdnc_library.domain.book.model;
 
 public enum BookGroup {
 
-    GROUP_T("T"),
-    GROUP_A("A"),
-    GROUP_M("M"),
-    GROUP_N("N"),
-    GROUP_A2("A2");
-
-    private final String value;
-
-    BookGroup(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
+    GROUP_T,
+    GROUP_A,
+    GROUP_M,
+    GROUP_N,
+    GROUP_A2,
 }
