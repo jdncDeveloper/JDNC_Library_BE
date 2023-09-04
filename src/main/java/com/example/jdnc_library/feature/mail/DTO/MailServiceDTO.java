93c4107406d6082bfff15125b3340efa23a627d4
simsimhekoon <37258541+simsimhekoon@users.noreply.github.com>
2023-09-04 14:35:49 +0900

    메일 서비스 async추가 및 기능 변경

---
+++ src/main/java/com/example/jdnc_library/feature/mail/DTO/MailServiceDTO.java
@@ -0,0 +1,10 @@
+package com.example.jdnc_library.feature.mail.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MailServiceDTO {
+    private long id;
+}
--- src/main/java/com/example/jdnc_library/feature/mail/contorller/SendMailController.java
+++ src/main/java/com/example/jdnc_library/feature/mail/contorller/SendMailController.java
@@ -1,23 +1,28 @@
 package com.example.jdnc_library.feature.mail.contorller;
 
+import com.example.jdnc_library.feature.mail.DTO.MailServiceDTO;
 import com.example.jdnc_library.feature.mail.service.EmailService;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequiredArgsConstructor
-public class SendMailTestController {
+@EnableAsync
+public class SendMailController {
 
     private final EmailService emailService;
 
     @PostMapping("/mail")
     @ResponseStatus(HttpStatus.OK)
-    public void SendMailTest() throws MessagingException {
-        emailService.SendMail();
+    public void sendMailTest(@RequestBody MailServiceDTO mailServiceDTO) throws MessagingException {
+        emailService.sendMail(mailServiceDTO.getId());
     }
 
 }
